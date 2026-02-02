f7ffbbda29a156dc6ac95060495657c53704cc47
Chamod Sathsara <sathsarawapc@gmail.com>
2026-02-02 12:54:11 +0530

    come after update

---
+++ app/customer-feedback-machines/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react';
+import CustomerReviewSystem from '@/components/customer/CustomerReviewSystem';
+
+export default function CustomerDashboard() {
+  return (
+    <div>
+      <CustomerReviewSystem />
+    </div>
+  );
+}
